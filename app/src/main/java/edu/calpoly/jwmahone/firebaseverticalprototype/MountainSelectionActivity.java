1c7663b51e6f03de66876a6808248a6fcc258a8c
jmahoney <jwmahoney13@gmail.com>
2016-06-02 13:14:41 -0700

    added UI styling/elevation to the views

--- app/src/main/java/edu/calpoly/jwmahone/firebaseverticalprototype/MountainSelectionActivity.java
+++ app/src/main/java/edu/calpoly/jwmahone/firebaseverticalprototype/MountainSelectionActivity.java
@@ -13,6 +13,8 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MountainSelectionActivity extends AppCompatActivity {
@@ -25,6 +27,10 @@ public class MountainSelectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mountain_selection);
+
+        //DatabaseReference testRef = FirebaseDatabase.getInstance().getReference();
+        //testRef.child("testing").push().setValue("test");
+
 
         mAuth = FirebaseAuth.getInstance();
 
